a7595f81e39512d1c1dc08bf13ea4b7aab4da6e5
cultured-collection-8468 <patel1357@gmail.com>
2019-01-31 16:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if sayHiToGrandma(string) === uppercase.toUpperCase()
+  if sayHiToGrandma(string) === uppercase.toUpperCase()){
   then
 }
